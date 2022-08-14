54356c29901def0a35dce63570e702ba8c1c642b
gotoa1234 <cap8825@gmail.com>
2022-08-15 00:20:50 +0800

    1. 調整備註

--- 0005_Longest_Palindromic_Substring.cpp
+++ 0005_Longest_Palindromic_Substring.cpp
@@ -28,52 +28,146 @@ namespace Solution5
 #pragma endregion Model
 
 #pragma region Main
-    public:
+	public:
         string longestPalindrome(string s) {
-            string result(1, s[0]);
-            unordered_map<int, int> hashMap;
-            int currentMax = 0;
-            int stringLength = s.size();
-            for (int index = 0; index < stringLength; index++)
+            int stringMaxLength = s.length();
+            if (stringMaxLength <= 1)
             {
-                if (hashMap.find(s[index]) == hashMap.end())
+                return s;
+            }
+
+            //Step 1 : 填充字元# - 解決奇偶處理
+            string s2 = "#";
+            for (int i = 0; i < stringMaxLength; i++)
+            {
+                s2.push_back(s[i]);
+                s2.push_back('#');
+            }
+
+            //Step 2：
+            int cx = 0, currentMax = 0;
+            const int s2StringMaxLength = s2.size();
+            vector<int> dynamicPrograming(s2StringMaxLength, 0);
+
+            //s2[0] 是# 字元，故從index = 1 開始
+            for (int index = 1; index < s2StringMaxLength; index++)
+            {
+                int moveableIndex = 0;//觀察右邊字元索引，往前看的數量
+                if (index >= currentMax)
                 {
-                    currentMax++;
+                    moveableIndex = index + 1;
                 }
                 else
                 {
-                    currentMax = index - hashMap[s[index]];
-					string sub = GetSubString(s, hashMap[s[index]], currentMax + 1);
-                    if (sub.size() > result.size() && IsPalindrome(sub))
+                    int ii = 2 * cx - index;
+                    dynamicPrograming[index] = min(currentMax - index, dynamicPrograming[ii]);
+                    if (ii - dynamicPrograming[ii] <= cx - dynamicPrograming[cx])
+                        moveableIndex = currentMax + 1;
+                }
+
+                for (int leftIndex = 2 * index - moveableIndex, rightIndex = moveableIndex;
+                    moveableIndex && moveableIndex < s2StringMaxLength && leftIndex >= 0;
+                    leftIndex = 2 * index - moveableIndex, rightIndex = moveableIndex)
+                {
+                    if (s2[leftIndex] == s2[rightIndex])
                     {
-                        result = sub;
+                        dynamicPrograming[index]++;
+                        moveableIndex++;
+                    }
+                    else
+                    {
+                        break;
                     }
                 }
-                hashMap[s[index]] = index;
-            }
-            string sub = GetSubString(s, hashMap[s[stringLength]], (stringLength - hashMap[s[stringLength]] + 1));
-            if (sub.size() > result.size() && IsPalindrome(sub))
-            {
-                result = sub;
-            }
-            return result;
-        }
-    private:
-        string GetSubString(string& sourceString, int startIndex, int length)
-        {
-            return sourceString.substr(startIndex, length);
-        };
-        bool IsPalindrome(string subString)
-        {
-            int maxLength = subString.size();
-            for (int headIndex = 0, lastIndex = maxLength-1; headIndex < lastIndex; headIndex++ , lastIndex--)
-            {
-                if (subString[headIndex] != subString[lastIndex])
+
+                while (
+                    moveableIndex &&
+                    (moveableIndex < s2StringMaxLength) &&
+                    (2 * index - moveableIndex >= 0) &&
+                    (s2[moveableIndex] == s2[2 * index - moveableIndex])
+                    )
                 {
-                    return false;
+                    dynamicPrograming[index]++;
+                    moveableIndex++;
+                }
+
+                if (index + dynamicPrograming[index] > currentMax)
+                {
+                    currentMax = index + dynamicPrograming[index];
+                    cx = index;
                 }
             }
-            return true;
+            
+            //檢索出DP裡面最長的回文 與 該字元起始索引位置
+            int targeId = 0;
+			for (int index = 1, maxlength = 0; index < s2StringMaxLength; index++)
+			{
+				if (dynamicPrograming[index] > maxlength)
+				{
+					maxlength = dynamicPrograming[index];
+					targeId = index;
+				}
+			}
+
+            //跳過 # 字元，組成回原本的回文子字串
+			string result;
+            for (int Index = targeId - dynamicPrograming[targeId] + 1; 
+                Index <= targeId + dynamicPrograming[targeId]; 
+                Index += 2
+                )
+            {
+                result.push_back(s2[Index]);
+            }
+            return result;
+		};
+
+    /// <summary>
+    /// 思路：回文解法一般人通解: 每個字元遍歷，每次遍歷時將字元 自己 + 下一個，丟進函式中，該字元往左往右不斷跑直到找到最長回文
+    /// 效能： O(n^2)
+    ///      Runtime :13 ms, faster than 94.30 % of C++ online submissions for Longest Palindromic Substring.
+    /// Memory Usage : 6.9 MB, less than 97.46 % of C++ online submissions for Longest Palindromic Substring.
+    /// </summary>
+    public:
+        string longestPalindrome_Solution2(string s) {
+            int maxSize = s.length();
+            int resultIndex{};
+            int resultCount{};
+
+            for (int index{}; index < maxSize; index++) 
+            {
+                //關鍵1: 往左右檢索 => 奇(自己為中心點) ; 偶(自己+下一個位置)
+                GetPalindromeString_Solution2(s, resultIndex, resultCount, index, index);
+                GetPalindromeString_Solution2(s, resultIndex, resultCount, index, index + 1);
+
+                //關鍵2: 檢索過程中找到很長的資料，可以將索引基準點往最後的索引前一個位置，因為此段已經在長度範圍中
+                //       避免無謂的檢索浪費
+                if ((resultIndex - 1) > index)
+                {
+                    //基準點可往後
+                    index = resultIndex - 1;
+                }
+            }
+            return s.substr(resultIndex, resultCount);
+        }
+    private:
+        void GetPalindromeString_Solution2(string& sourceString,
+            int& resultIndex,
+            int& resultCount,
+            int startIndex, 
+            int lastIndex)
+        {
+			for (; 0 <= startIndex && lastIndex < sourceString.size(); startIndex--, lastIndex++)
+            {
+                if (sourceString[startIndex] != sourceString[lastIndex])
+                {
+                    break;
+                }
+                if ((lastIndex - startIndex + 1) > resultCount)
+                {
+                    resultIndex = startIndex;
+                    resultCount = (lastIndex - startIndex + 1);
+                }
+            }
         };
 #pragma endregion Main
 
@@ -85,7 +179,7 @@ namespace Solution5
         Longest_Palindromic_Substring_Model GetTestData001(void)
         {
             Longest_Palindromic_Substring_Model result;
-            result.s = "xaabacxcabaaxcabaax";// "babad";
+            result.s = "sadxaabacxcabaaxcabaax";// "babad";
             return result;
         };
 
--- LeetCodeProblemSolvingBy_C_Double_Plus.cpp
+++ LeetCodeProblemSolvingBy_C_Double_Plus.cpp
@@ -1,10 +1,53 @@
 ﻿#include <vector>
-#include "0001_two_sum.cpp"
-using namespace Solution1;
+#include "0005_Longest_Palindromic_Substring.cpp"
+using namespace Solution5;
+using namespace std;
+
 int main()
 {
-    Solution1::Two_Sum useClass;
-    Solution1::Two_Sum::Two_Sum_Model getTestModel = useClass.GetTestData001();
-    vector<int> result = useClass.twoSum(getTestModel.nums, getTestModel.target);
+    for (int i = 0; i < 10; i++)
+    {
+        if (i)
+        {
+            cout << "沒有" << endl;
+        }
+        else
+        {
+            cout << "有" << endl;
+        }
+        
+    }
+
+    Solution5::Longest_Palindromic_Substring useClass;
+    Solution5::Longest_Palindromic_Substring::Longest_Palindromic_Substring_Model getTestModel = useClass.GetTestData001();
+    string result = useClass.longestPalindrome(getTestModel.s);
+
+    string result2 = useClass.longestPalindrome_Solution2(getTestModel.s);
     return 0;
 }
+
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int n = s.length();
+        int st{}, stmax{};
+        int mx{};
+        for (int i{}; i < n; i++) {
+            st = i;
+            while (i < n && st >= 0 && s[i] == s[st]) {
+                i++;
+            }
+            i--;
+            int j{};
+            while (i + j < n && st - j >= 0 && s[i + j] == s[st - j]) {
+                j++;
+            }
+            j--;
+            if (mx < (i - st + 2 * j + 1)) {
+                mx = i - st + 2 * j + 1;
+                stmax = st - j;
+            }
+        }
+        return s.substr(stmax, mx);
+    }
+};
