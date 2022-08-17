2e6ad571049e86af9e818d1aca425f0485968c6d
gotoa1234 <cap8825@gmail.com>
2022-08-17 21:54:14 +0800

    1. 增加 0009.Palindrome_Number 檢查是否為回文整數

---
+++ 0009.Palindrome_Number.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+using namespace std;
+
+namespace Solution9
+{
+#pragma region Paste to execute 
+    /*
+    #include "0009.Palindrome_Number.cpp"
+    using namespace Solution9;
+    using namespace std;
+
+    Solution9::Palindrome_Number useClass;
+    Solution9::Palindrome_Number::Palindrome_Number_Model getTestModel = useClass.GetTestData001();
+    bool result = useClass.myAtoi(getTestModel.x);
+    */
+#pragma endregion Paste to execute
+
+    /// <summary>
+    /// 是否為回文整數
+    /// ※轉成字串太簡單，LeetCode建議不用String嘗試挑戰
+    /// </summary>
+    class Palindrome_Number
+    {
+#pragma region Model
+    public:
+        class Palindrome_Number_Model
+        {
+        public:
+            int x;
+        };
+#pragma endregion Model
+
+#pragma region Main
+    public:
+        /// <summary>
+        ///          思路：排除負數/尾0 => 整數倒換 => 溢位判斷 => 最後判斷整數倒換值是否與原值相同
+        ///      Runtime :  12 ms, faster than 87.84 % of C++ online submissions for Palindrome Number.
+        /// Memory Usage : 5.8 MB,   less than 91.05 % of C++ online submissions for Palindrome Number.
+        /// </summary>
+        bool myAtoi(int x) {
+            if (x < 0)
+                return false;
+            if (x != 0 && x % 10 == 0)
+                return false;
+
+            int tmep = x;
+            int result = 0;
+            int remainder = 0;
+            while (tmep)
+            {
+                remainder = tmep % 10;
+                if (result > (INT_MAX - (remainder)) / 10)
+                {
+                    return false;
+                }
+                result = result * 10 + remainder;
+                tmep = tmep / 10;
+            }
+            return result == x ? true : false;
+        };
+#pragma endregion Main
+
+#pragma region TestData
+    public:
+        /// <summary>
+        /// 測試資料1
+        /// </summary>        
+        Palindrome_Number_Model GetTestData001(void)
+        {
+            Palindrome_Number_Model result;
+            result.x = 121;//
+            return result;
+        };
+
+        /// <summary>
+        /// 測試資料2
+        /// </summary>   
+        Palindrome_Number_Model GetTestData002(void)
+        {
+            Palindrome_Number_Model result;
+            result.x = -121;
+            return result;
+        };
+
+        /// <summary>
+        /// 測試資料3
+        /// </summary>   
+        Palindrome_Number_Model GetTestData003(void)
+        {
+            Palindrome_Number_Model result;
+            result.x = 10;
+            return result;
+        };
+#pragma endregion TestData
+    };
+}
--- LeetCodeProblemSolvingBy_C_Double_Plus.cpp
+++ LeetCodeProblemSolvingBy_C_Double_Plus.cpp
@@ -1,40 +1,11 @@
-﻿#include <vector>
-#include "0008.String_To_Integer_Atoi.cpp"
-using namespace Solution8;
+﻿#include "0009.Palindrome_Number.cpp"
+using namespace Solution9;
 using namespace std;
 
 int main()
 {
-
-
-    Solution8::String_To_Integer_Atoi useClass;
-    Solution8::String_To_Integer_Atoi::String_To_Integer_Atoi_Model getTestModel = useClass.GetTestData001();
-    int result = useClass.myAtoi(getTestModel.s);
-    return 0;
+    Solution9::Palindrome_Number useClass;
+	Solution9::Palindrome_Number::Palindrome_Number_Model getTestModel = useClass.GetTestData001();
+	bool result = useClass.myAtoi(getTestModel.x);
+	return 0;
 }
-
-class Solution {
-public:
-    string longestPalindrome(string s) {
-        int n = s.length();
-        int st{}, stmax{};
-        int mx{};
-        for (int i{}; i < n; i++) {
-            st = i;
-            while (i < n && st >= 0 && s[i] == s[st]) {
-                i++;
-            }
-            i--;
-            int j{};
-            while (i + j < n && st - j >= 0 && s[i + j] == s[st - j]) {
-                j++;
-            }
-            j--;
-            if (mx < (i - st + 2 * j + 1)) {
-                mx = i - st + 2 * j + 1;
-                stmax = st - j;
-            }
-        }
-        return s.substr(stmax, mx);
-    }
-};
