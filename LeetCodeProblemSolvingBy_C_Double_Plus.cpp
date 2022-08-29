f45a80d13b0789c29a2f6f93d333651f99a6c9a1
gotoa1234 <cap8825@gmail.com>
2022-08-29 17:52:18 +0800

    暫存

--- LeetCodeProblemSolvingBy_C_Double_Plus.cpp
+++ LeetCodeProblemSolvingBy_C_Double_Plus.cpp
@@ -1,14 +1,71 @@
 ﻿#include <vector>
+#include <unordered_map>
 #include "0424.Longest_Repeating_Character_Replacement.cpp"
+#include <string>
 using namespace Solution424;
+string getHint(string secret, string guess) {
+	unordered_map<char, int> hashMap;
+	for (int index = 0; index < secret.size(); index++)
+	{
+		hashMap[secret[index]]++;
+	}
+
+	int countA = 0;
+	int countB = 0;
+	for (int index = 0; index < guess.size(); index++)
+	{
+		if (hashMap.find(guess[index]) != hashMap.end())
+		{
+			//相同+A
+			if (guess[index] == secret[index])
+			{
+				countA++;
+				if (hashMap[guess[index]] > 0)
+					hashMap[guess[index]]--;
+				else
+					countB--;
+			}
+			else//不相同+B
+			{
+				if (hashMap[guess[index]] > 0)
+				{
+					countB++;
+					int a= hashMap[guess[index]];
+					hashMap[guess[index]]--;
+					int b = hashMap[guess[index]];
+				}
+			}
+		}
+	}
+	return to_string(countA) + "A" + to_string(countB) + "B";
+}
 
 int main()
 {
-    Solution424::Longest_Repeating_Character_Replacement useClass;
-    Solution424::Longest_Repeating_Character_Replacement::Longest_Repeating_Character_Replacement_Model getTestModel = useClass.GetTestData001();
-    int result = useClass.characterReplacement(getTestModel.s, getTestModel.k);
+	string secret = "112344", guess = "011146";
+	string result = getHint(secret, guess);
+	/*std::string res;
+	std::array<int, 10> fs{}, fg{};
+	int b = 0, c = 0, n = secret.size();
 
-    getTestModel = useClass.GetTestData002();
-    result = useClass.characterReplacement(getTestModel.s, getTestModel.k);
-    return 0;
+	for (int i = 0; i < n; ++i) {
+		if (secret[i] == guess[i]) {
+			++b;
+		}
+		else {
+			++fs[secret[i] - '0'];
+			++fg[guess[i] - '0'];
+		}
+	}
+
+	for (int i = 0; i < 10; ++i) {
+		c += std::min(fs[i], fg[i]);
+	}
+
+	res += std::to_string(b);
+	res.push_back('A');
+	res += std::to_string(c);
+	res.push_back('B');
+
+	return res;*/
 }
