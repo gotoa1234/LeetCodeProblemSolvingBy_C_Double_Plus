ad1a7b586f1e5b4ced0d723622f0cad287170b5c
gotoa1234 <cap8825@gmail.com>
2022-09-06 22:32:36 +0800

    1. 優化14代碼

--- 0014.Longest_Common_Prefix.cpp
+++ 0014.Longest_Common_Prefix.cpp
@@ -42,25 +42,22 @@ namespace Solution14
 #pragma region Main
 	public:
 		/// <summary>
-		///          思路：排序，第一個必定是最小長度，然後對每個項目比對，若都相同則記錄，當不相同時跳出
+		///          思路：利用C++的Sort排序，第一個必定是最小長度，然後對頭尾兩個字串比對char，若都相同則記錄，當不相同時跳出
 		///      Runtime :   3 ms, faster than 92.55% of C++ online submissions for Longest Common Prefix.
 		/// Memory Usage : 9.2 MB,   less than 83.20% of C++ online submissions for Longest Common Prefix.
 		/// </summary>
 		string longestCommonPrefix(vector<string>& strs) {
 			string result = "";
-			char currentChar = 0;
-			short moveIndex = 0;			
 			short index = 0;
 			sort(strs.begin(), strs.end());
 			short minLength = strs[0].size();
-			while (moveIndex < minLength)
+			for (index = 0; index < strs[0].size(); index++)
 			{
-				currentChar = strs[0][moveIndex];
-				for (index = 0; index < strs.size() && currentChar == strs[index][moveIndex]; index++) {}
-				if (index != strs.size())
+				if (strs[0][index] != strs[strs.size() - 1][index])
+				{
 					break;
-				result.push_back(currentChar);
-				moveIndex++;
+				}
+				result.push_back(strs[strs.size() - 1][index]);
 			}
 			return result;
 		}
@@ -74,7 +71,7 @@ namespace Solution14
 		Longest_Common_Prefix_Model GetTestData001(void)
 		{
 			Longest_Common_Prefix_Model result;
-			result.strs = { "flower","flow","flight" };
+			result.strs = { "flo","flsw","floght" };
 			return result;//except: "fl"
 		};
 
