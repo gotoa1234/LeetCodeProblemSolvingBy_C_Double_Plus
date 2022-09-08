5087f96f5c4e766661dc400ba5f77fe9821e7d4a
gotoa1234 <cap8825@gmail.com>
2022-09-09 01:11:44 +0800

    1. 理解3Sum

--- 0015.3Sum.cpp
+++ 0015.3Sum.cpp
@@ -44,58 +44,56 @@ namespace Solution15
 #pragma region Main
 	public:
 		/// <summary>
-		///          思路：
-		///      Runtime : 
-		/// Memory Usage : 
+		///          思路：排序，然後遍歷將最左邊的數值(0)固定，然後將(1)與最後值(nums.size())，三個相加，若結果小於0 將(1) -> (2);若大於0則
+		///                (nums.size() -1) 當交界時遍歷起點為(1) ......重複後取得結果
+		///      Runtime:   128 ms, faster than 64.67% of C++ online submissions for 3Sum.
+		/// Memory Usage : 19.8 MB,   less than 95.65% of C++ online submissions for 3Sum.
 		/// </summary>
 		vector<vector<int>> ThreeSum(vector<int>& nums) {
 			vector<vector<int>> result;
-			sort(nums.begin(), nums.end());
 			int leftIndex = 0;
-			int leftValue = 0;
+			int rightIndex = 0;
 			int middleValue = 0;
 			int rightValue = 0;
-			int useIndex = 0;
-			for (int leftIndex = 0, rightIndex = nums.size() - 1; leftIndex <= nums.size() - 3; leftIndex++)
+			int numsMax = nums.size();
+			//1. 排序
+			sort(nums.begin(), nums.end());
+			//2. 遍歷
+			for (int index = 0; index < numsMax; index++)
 			{
-				int startIndex = leftIndex;
-				rightIndex = nums.size() - 1;
-				leftValue = nums[startIndex];
-				rightValue = nums[rightIndex];
-				if (leftValue + rightValue >= 0)
-				{
-					startIndex++;
-					useIndex = startIndex;
-				}
-				else
-				{
-					rightIndex--;
-					useIndex = rightIndex;
-				}
+				//3.選擇兩個點(最左、最右)
+				leftIndex = index + 1;
+				rightIndex = numsMax - 1;
 
-				
-				while (startIndex < rightIndex)
+				//當交集時跳出
+				while (leftIndex < rightIndex)
 				{
-					middleValue = nums[useIndex];
-					if (leftValue + rightValue + middleValue == 0)
+					middleValue = nums[leftIndex];
+					rightValue = nums[rightIndex];
+					if (nums[index] + middleValue + rightValue == 0)
 					{
-						result.push_back({ leftValue ,middleValue, rightValue });
-					}
+						//找到時做紀錄
+						result.push_back({ nums[index] ,middleValue , rightValue });
 
-					if (leftValue + rightValue >= 0)
-					{
-						startIndex++;
-						useIndex = startIndex;
+						//以下是找出相同數值時往右與往左
+						int tempMiddleValue = nums[leftIndex];
+						int tempRightValue = nums[rightIndex];
+						while (leftIndex < rightIndex && nums[leftIndex] == tempMiddleValue)
+							leftIndex++;
+						while (rightIndex > 0 && nums[rightIndex] == tempRightValue)
+							rightIndex--;
 					}
-					else
-					{
+					else if (nums[index] + middleValue + rightValue < 0)//小於0，往右讓數值變大，趨近0
+						leftIndex++;
+					else//大於0，往左讓數值變小，趨近0
 						rightIndex--;
-						useIndex = rightIndex;
-					}
 				}
+				//以下是找出起點相同數值時往右
+				while (index + 1 < numsMax && nums[index + 1] == nums[index])
+					index++;
 			}
 			return result;
-		}
+		}		
 #pragma endregion Main
 
 #pragma region TestData
