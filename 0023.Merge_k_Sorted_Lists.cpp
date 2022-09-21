af83beed659a0dca39e6edc682472f52752b69ff
gotoa1234 <cap8825@gmail.com>
2022-09-21 14:06:35 +0800

    1. 完成0023 最優解

--- 0023.Merge_k_Sorted_Lists.cpp
+++ 0023.Merge_k_Sorted_Lists.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <queue>
 using namespace std;
 
 namespace Solution23
 {
 #pragma region Paste to execute 
 	/*
-	#include "0023.Merge_k_Sorted_Lists"
+	#include "0023.Merge_k_Sorted_Lists.cpp"
 	using namespace Solution23;
 	using namespace std;
 
@@ -15,13 +16,13 @@ namespace Solution23
 	{
 		Solution23::Merge_k_Sorted_Lists useClass;
 		Solution23::Merge_k_Sorted_Lists::Merge_k_Sorted_Lists_Model getTestModel = useClass.GetTestData001();
-		Solution23::Merge_k_Sorted_Lists::ListNode* result = useClass.swapPairs(getTestModel.head);
+		Solution23::Merge_k_Sorted_Lists::ListNode* result = useClass.mergeKLists(getTestModel.head);
 
 		getTestModel = useClass.GetTestData002();
-		result = useClass.swapPairs(getTestModel.head);
+		result = useClass.mergeKLists(getTestModel.head);
 
 		getTestModel = useClass.GetTestData003();
-		result = useClass.swapPairs(getTestModel.head);
+		result = useClass.mergeKLists(getTestModel.head);
 		return 0;
 	}
 	*/
@@ -30,7 +31,7 @@ namespace Solution23
 	/// <summary>
 	/// 成對交換節點
 	/// </summary>
-	class Swap_Nodes_in_Pairs
+	class Merge_k_Sorted_Lists
 	{
 #pragma region Model
 	public:
@@ -43,23 +44,60 @@ namespace Solution23
 			ListNode(int x, ListNode* next) : val(x), next(next) {}
 		};
 	public:
-		class Swap_Nodes_in_Pairs_Model
+		class Merge_k_Sorted_Lists_Model
 		{
 		public:
-			ListNode* head;
+			vector<ListNode*> head;
 		};
 #pragma endregion Model
 
 #pragma region Main
 	public:
 		/// <summary>
-		///         思路：
-		///      Runtime:  
-		/// Memory Usage : 
+		///         思路：利用heap-min 最小堆積的 priority_queue 排序所有項目的鍊 -> 接著遍歷priority_queue 每次取出最小的鍊的節點 ->
+		///               然後再依序取出組成一維節點  -> 輸出
+		///      Runtime :   24 ms, faster than 92.87% of C++ online submissions for Merge k Sorted Lists.
+		/// Memory Usage : 13.2 MB,   less than 83.50% of C++ online submissions for Merge k Sorted Lists.
 		/// </summary>
-		ListNode* swapPairs(ListNode* head) {
-			return nullptr;
+		ListNode* mergeKLists(vector<ListNode*>& lists)
+		{
+			//1. 自定義比較
+			auto cmp = [](ListNode*& nodeA, ListNode*& nodeB)
+			{
+				//節點A 大於B 時為
+				return nodeA->val > nodeB->val;
+			};
+
+			//2. 使用優先權 queue 做
+			priority_queue<ListNode*, vector<ListNode*>, decltype(cmp)> minHeapQueue(cmp);
+			for (auto node : lists)
+			{
+				if (node) //鍊不可為Null，需要帶有值的Node
+				{
+					//每次都會將 node 與 現在的節點做排序，將第一個最小值的鍊放在最前面
+					minHeapQueue.push(node);
+				}
+			}
+
+			//3. 把 min heap 的結果依序取出，組成新的一維節點
+			ListNode* headNode = new ListNode(-1);
+			ListNode* moveNode = headNode;
+			while (!minHeapQueue.empty())
+			{
+				auto getTopNode = minHeapQueue.top();
+				minHeapQueue.pop();
+				//每次取出1個最小的val節點
+				moveNode->next = getTopNode;
+				moveNode = moveNode->next;
+				if (moveNode->next)
+				{
+					//push時會將最小val的鍊放在[0]位置
+					minHeapQueue.push(moveNode->next);
+				}
+			}
+			return headNode->next;
 		}
+
 #pragma endregion Main
 
 #pragma region TestData
@@ -67,34 +105,43 @@ namespace Solution23
 		/// <summary>
 		/// 測試資料1
 		/// </summary>        
-		Swap_Nodes_in_Pairs_Model GetTestData001(void)
+		Merge_k_Sorted_Lists_Model GetTestData001(void)
 		{
-			Swap_Nodes_in_Pairs_Model result;
-			vector<int> input = { 1,2,3,4 };
-			result.head = ConstructNodes(input);
-			return result;//except: [2,1,4,3]
+			Merge_k_Sorted_Lists_Model result;
+			//[[1, 4, 5], [1, 3, 4], [2, 6]]
+			vector<ListNode*> input;
+			vector<int> temp = { 1, 4, 5 };
+			input.push_back(ConstructNodes(temp));
+			temp = { 1, 3, 4 };
+			input.push_back(ConstructNodes(temp));
+			temp = { 2,6 };
+			input.push_back(ConstructNodes(temp));
+			result.head = input;
+			return result;//except: [1,1,2,3,4,4,5,6]
 		};
 
 		/// <summary>
 		/// 測試資料2
 		/// </summary>   
-		Swap_Nodes_in_Pairs_Model GetTestData002(void)
+		Merge_k_Sorted_Lists_Model GetTestData002(void)
 		{
-			Swap_Nodes_in_Pairs_Model result;
-			vector<int> input = {};
-			result.head = nullptr;
+			Merge_k_Sorted_Lists_Model result;
+			vector<ListNode*> input;
+			result.head = input;
 			return result;//expect:[]
 		};
 
 		/// <summary>
 		/// 測試資料3
 		/// </summary>   
-		Swap_Nodes_in_Pairs_Model GetTestData003(void)
+		Merge_k_Sorted_Lists_Model GetTestData003(void)
 		{
-			Swap_Nodes_in_Pairs_Model result;
-			vector<int> input = { 1 };
-			result.head = ConstructNodes(input);
-			return result;//[1]
+			Merge_k_Sorted_Lists_Model result;
+			vector<ListNode*> input;
+			vector<int> temp = {};
+			input.push_back(ConstructNodes(temp));
+			result.head = input;
+			return result;//[]
 		};
 #pragma endregion TestData
 	private:
--- LeetCodeProblemSolvingBy_C_Double_Plus.cpp
+++ LeetCodeProblemSolvingBy_C_Double_Plus.cpp
@@ -1,15 +1,17 @@
-﻿#include "0022.Generate_Parentheses.cpp"
-using namespace Solution22;
+﻿#include "0023.Merge_k_Sorted_Lists.cpp"
+using namespace Solution23;
 using namespace std;
 
 int main()
 {
-	Solution22::Generate_Parentheses useClass;
-	Solution22::Generate_Parentheses::Generate_Parentheses_Model getTestModel = useClass.GetTestData001();
-	vector<string> result = useClass.generateParenthesis(getTestModel.n);
+	Solution23::Merge_k_Sorted_Lists useClass;
+	Solution23::Merge_k_Sorted_Lists::Merge_k_Sorted_Lists_Model getTestModel = useClass.GetTestData001();
+	Solution23::Merge_k_Sorted_Lists::ListNode* result = useClass.mergeKLists(getTestModel.head);
 
 	getTestModel = useClass.GetTestData002();
-	result = useClass.generateParenthesis(getTestModel.n);
+	result = useClass.mergeKLists(getTestModel.head);
 
+	getTestModel = useClass.GetTestData003();
+	result = useClass.mergeKLists(getTestModel.head);
 	return 0;
 }
