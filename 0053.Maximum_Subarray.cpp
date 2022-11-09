77b45d4c21b3ef34b81ac0f0c26f9634d7f9d90d
gotoa1234 <cap8825@gmail.com>
2022-11-09 10:35:52 +0800

    1. 完成0053

--- 0053.Maximum_Subarray.cpp
+++ 0053.Maximum_Subarray.cpp
@@ -16,10 +16,10 @@ namespace Solution53
 
 		Solution53::Maximum_Subarray useClass;
 		Solution53::Maximum_Subarray::Maximum_Subarray_Model getTestModel = useClass.GetTestData001();
-		auto result1 = useClass.maxSubArray(getTestModel.n);
+		auto result1 = useClass.maxSubArray(getTestModel.nums);
 
 		getTestModel = useClass.GetTestData002();
-		auto result2 = useClass.maxSubArray(getTestModel.n);
+		auto result2 = useClass.maxSubArray(getTestModel.nums);
 		return 0;
 	}
 
@@ -44,12 +44,42 @@ namespace Solution53
 #pragma region Main
 	public:
 		/// <summary>
-		///     thinking： 
-		///       Runtime：
-		/// Memory Usage ：
+		///     thinking： 可以利用內積法，每次運算將上次的結果做下次
+		///                以此為例 { -2,1,-3,4,-1,2,1,-5,4 }
+		///                  Step 1:  (-2 + 1) 與 1  哪個大?  索引[1] 的 1大，所以保留替換 [1] 的位置
+		///                  Step 2:  (1  + -3) 與 -3 哪個大?  相加的-2大，所以保留替換 [2] 的位置
+		///                  Step 3:  (-2 +  4) 與 4 哪個大?  索引[3] 的 4大，所以保留替換 [3] 的位置
+		///                  Loop  :  直到最後一個索引就是最大值
+		///       Runtime： 143 ms Beats 80.78 %
+		/// Memory Usage ：67.6 MB Beats 90.92 %
 		/// </summary>	
 		int maxSubArray(vector<int>& nums) {
-			return 0;
+			int calculationValue = nums[0];
+			int maxResult = calculationValue;
+			for (int index = 1; index < nums.size(); index++)
+			{
+				calculationValue = max(calculationValue + nums[index], nums[index]);
+				maxResult = max(calculationValue, maxResult);
+			}
+			return maxResult;
+		}
+
+		/// <summary>
+		/// 動態規劃解
+		///       Runtime： 123 ms Beats 90.57 %
+		/// Memory Usage ：70.5 MB Beats  5.30 %
+		/// </summary>
+		/// <returns></returns>
+		int maxSubArray_DpSolve(vector<int>& nums) {
+			vector<int> dp(nums.size());
+			dp[0] = nums[0];
+			int maxResult = dp[0];
+			for (int index = 1; index < nums.size(); index++)
+			{
+				dp[index] = max(dp[index - 1] + nums[index], nums[index]);
+				maxResult = max(dp[index], maxResult);
+			}
+			return maxResult;
 		}
 
 #pragma endregion Main
--- LeetCodeProblemSolvingBy_C_Double_Plus.cpp
+++ LeetCodeProblemSolvingBy_C_Double_Plus.cpp
@@ -1,15 +1,15 @@
-﻿#include "0052.N_Queens_II.cpp"
-using namespace Solution52;
+﻿#include "0053.Maximum_Subarray.cpp"
+using namespace Solution53;
 using namespace std;
 
 int main()
 {
 
-	Solution52::N_Queens_II useClass;
-	Solution52::N_Queens_II::N_Queens_II_Model getTestModel = useClass.GetTestData001();
-	auto result1 = useClass.totalNQueens(getTestModel.n);
+	Solution53::Maximum_Subarray useClass;
+	Solution53::Maximum_Subarray::Maximum_Subarray_Model getTestModel = useClass.GetTestData001();
+	auto result1 = useClass.maxSubArray(getTestModel.nums);
 
 	getTestModel = useClass.GetTestData002();
-	auto result2 = useClass.totalNQueens(getTestModel.n);
+	auto result2 = useClass.maxSubArray(getTestModel.nums);
 	return 0;
 }
