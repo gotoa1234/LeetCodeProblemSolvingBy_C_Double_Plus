6aae579371f70a366695209ff80484f3174e1778
gotoa1234 <cap8825@gmail.com>
2022-11-15 09:39:26 +0800

    1.完成0058

---
+++ 0058.Length_of_Last_Word.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+#include <unordered_map>
+#include <algorithm>
+using namespace std;
+
+namespace Solution58
+{
+#pragma region Paste to execute 
+	/*
+	#include "0058.Length_of_Last_Word.cpp"
+	using namespace Solution58;
+	using namespace std;
+
+	int main()
+	{
+		Solution58::Length_of_Last_Word useClass;
+		Solution58::Length_of_Last_Word::Length_of_Last_Word_Model getTestModel = useClass.GetTestData001();
+		auto result1 = useClass.lengthOfLastWord(getTestModel.s);
+
+		getTestModel = useClass.GetTestData002();
+		auto result2 = useClass.lengthOfLastWord(getTestModel.s);
+
+		getTestModel = useClass.GetTestData003();
+		auto result3 = useClass.lengthOfLastWord(getTestModel.s);
+	}
+
+	*/
+#pragma endregion Paste to execute
+
+	/// <summary>
+	/// 找出字串最後一個單詞的長度
+	/// </summary>
+	class Length_of_Last_Word
+	{
+#pragma region Model
+	public:
+		class Length_of_Last_Word_Model
+		{
+		public:
+			string s;
+		};
+#pragma endregion Model
+
+#pragma region Main
+	public:
+		/// <summary>
+		///     thinking： 題目說 '小寫字母' 與 '空白字元' 那從最後一個索引開始
+		///                如果遇到第一個字元後，在遇到下個空白字元就是跳出的時候
+		///       Runtime：  3 ms Beats 71.96 %
+		/// Memory Usage ：6.5 MB Beats 89.60 %
+		/// </summary>	
+		int lengthOfLastWord(string s) {
+			int result = 0;
+			for (int index = s.size() - 1; 
+				index >= 0 && !(s[index] == ' ' &&  result > 0);
+				index--)
+			{
+				if (s[index] == ' ')
+					continue;
+				result++;
+			}
+			return result;
+		}
+#pragma endregion Main
+
+#pragma region TestData
+	public:
+		/// <summary>
+		/// test data 1
+		/// </summary>        
+		Length_of_Last_Word_Model GetTestData001(void)
+		{
+			Length_of_Last_Word_Model result;
+			result.s = "Hello World";
+			return result;//except:  5
+		};
+
+		/// <summary>
+		/// test data 2
+		/// </summary>        
+		Length_of_Last_Word_Model GetTestData002(void)
+		{
+			Length_of_Last_Word_Model result;
+			result.s = "   fly me   to   the moon  ";
+			return result;//except:  4
+		};
+
+		/// <summary>
+		/// test data 3
+		/// </summary>        
+		Length_of_Last_Word_Model GetTestData003(void)
+		{
+			Length_of_Last_Word_Model result;
+			result.s = "luffy is still joyboy";
+			return result;//except:  6
+		};
+#pragma endregion TestData
+
+	};
+}
--- LeetCodeProblemSolvingBy_C_Double_Plus.cpp
+++ LeetCodeProblemSolvingBy_C_Double_Plus.cpp
@@ -1,19 +1,16 @@
-﻿#include "0057.Insert_Interval.cpp"
-using namespace Solution57;
+﻿#include "0058.Length_of_Last_Word.cpp"
+using namespace Solution58;
 using namespace std;
 
 int main()
 {
-	Solution57::Insert_Interval useClass;
-	Solution57::Insert_Interval::Insert_Interval_Model getTestModel = useClass.GetTestData001();
-	auto result1 = useClass.insert(getTestModel.intervals, getTestModel.newInterval);
+	Solution58::Length_of_Last_Word useClass;
+	Solution58::Length_of_Last_Word::Length_of_Last_Word_Model getTestModel = useClass.GetTestData001();
+	auto result1 = useClass.lengthOfLastWord(getTestModel.s);
 
 	getTestModel = useClass.GetTestData002();
-	auto result2 = useClass.insert(getTestModel.intervals, getTestModel.newInterval);
+	auto result2 = useClass.lengthOfLastWord(getTestModel.s);
 
 	getTestModel = useClass.GetTestData003();
-	auto result3 = useClass.insert(getTestModel.intervals, getTestModel.newInterval);
-
-	getTestModel = useClass.GetTestData004();
-	auto result4 = useClass.insert(getTestModel.intervals, getTestModel.newInterval);
+	auto result3 = useClass.lengthOfLastWord(getTestModel.s);
 }
