7f28363504a03a6366ad370fa4e3fd29c7e0b320
gotoa1234 <cap8825@gmail.com>
2022-11-17 10:04:35 +0800

    1. 完成0061

--- 0061.Rotate_List.cpp
+++ 0061.Rotate_List.cpp
@@ -7,26 +7,34 @@ namespace Solution61
 {
 #pragma region Paste to execute 
 	/*
-	#include "0061.Rotate_List.cpp"
-	using namespace Solution61;
-	using namespace std;
-
-	int main()
-	{
-		Solution61::Rotate_List useClass;
-		Solution61::Rotate_List::Rotate_List_Model getTestModel = useClass.GetTestData001();
-		Solution61::Rotate_List::ListNode* result = useClass.reverseKGroup(getTestModel.head, getTestModel.k);
-
-		getTestModel = useClass.GetTestData002();
-		result = useClass.reverseKGroup(getTestModel.head, getTestModel.k);
-
-		return 0;
-	}
+    #include "0061.Rotate_List.cpp"
+    using namespace Solution61;
+    using namespace std;
+    
+    int main()
+    {
+    	Solution61::Rotate_List useClass;
+    	Solution61::Rotate_List::Rotate_List_Model getTestModel = useClass.GetTestData001();
+    	auto result1 = useClass.rotateRight(getTestModel.head, getTestModel.k);
+    
+    	getTestModel = useClass.GetTestData002();
+    	auto result2 = useClass.rotateRight(getTestModel.head, getTestModel.k);
+    
+    	getTestModel = useClass.GetTestData003();
+    	auto result3 = useClass.rotateRight(getTestModel.head, getTestModel.k);
+    
+    	getTestModel = useClass.GetTestData004();
+    	auto result4 = useClass.rotateRight(getTestModel.head, getTestModel.k);
+    
+    	getTestModel = useClass.GetTestData005();
+    	auto result5 = useClass.rotateRight(getTestModel.head, getTestModel.k);
+    	return 0;
+    }
 	*/
 #pragma endregion Paste to execute
 
 	/// <summary>
-	/// k-Group 中的反向節點
+	/// 旋轉串鍊
 	/// </summary>
 	class Rotate_List
 	{
@@ -52,13 +60,46 @@ namespace Solution61
 #pragma region Main
 	public:
 		/// <summary>
-		///         思路：
-		///      Runtime: 
-		/// Memory Usage :
+		///         思路：找出向右移動的規則
+		///               1. 遍歷找出串鍊總長度X ，可以得之 k % X 會是真實位置，並記錄尾巴節點
+		///               2. 再利用總長度 X - (k % X) 就會是起點Node
+		///               3. 將尾巴節點 指向到 頭節點，並且再走到 X - (k % X) 當起點 (此為答案)
+		///               Time : O(nlog n)
+		///      Runtime:    3 ms Beats 99.48 %
+		/// Memory Usage :11.7 MB Beats 55.34 %
 		/// </summary>
 	public:
 		ListNode* rotateRight(ListNode* head, int k) {
-			return { };
+            ListNode* fakeNode = head;
+			ListNode* moveNode = head;
+			ListNode* endNode = head;
+
+			//1. 找出總長、最後一個節點
+			int maxLength = 0;
+			while (moveNode)
+			{
+				endNode = moveNode;
+				moveNode = moveNode->next;
+				maxLength++;
+			}
+			
+			//2. 排除原點/不用計算的項目
+			if (k == 0 || head == nullptr || k % maxLength == 0)
+				return head;
+
+			//3. 將尾指向頭，並找出新的頭
+			ListNode* targetNode = head;
+			ListNode* leastTargetNode = head;
+			for (int moveStep = maxLength - (k % maxLength);
+				moveStep > 0;
+				moveStep--)
+			{
+				leastTargetNode = targetNode;
+				targetNode = targetNode->next;
+			}
+			leastTargetNode->next = nullptr;
+			endNode->next = head;
+			return targetNode;
 		}
 #pragma endregion Main
 
@@ -86,6 +127,33 @@ namespace Solution61
 			result.head = ConstructNodes(input);
 			result.k = 4;
 			return result;//expect:[2,0,1] 
+		};
+
+		Rotate_List_Model GetTestData003(void)
+		{
+			Rotate_List_Model result;
+			vector<int> input = { 1,2,3,4,5 };
+			result.head = ConstructNodes(input);
+			result.k = 4;
+			return result;//expect:[2,3,4,5,1]
+		};
+
+		Rotate_List_Model GetTestData004(void)
+		{
+			Rotate_List_Model result;
+			vector<int> input = {  };
+			result.head = ConstructNodes(input);
+			result.k = 0;
+			return result;//expect:[]
+		};
+
+		Rotate_List_Model GetTestData005(void)
+		{
+			Rotate_List_Model result;
+			vector<int> input = {  };
+			result.head = ConstructNodes(input);
+			result.k = 1;
+			return result;//expect:[]
 		};
 	private:
 		ListNode* ConstructNodes(vector<int>& inputDatas)
--- LeetCodeProblemSolvingBy_C_Double_Plus.cpp
+++ LeetCodeProblemSolvingBy_C_Double_Plus.cpp
@@ -1,39 +1,23 @@
-﻿
-#include "0060.Permutation_Sequence.cpp"
-#include <string>
-#include <numeric>
-using namespace Solution60;
+﻿#include "0061.Rotate_List.cpp"
+using namespace Solution61;
 using namespace std;
-
 
 int main()
 {
-    int N = 5;
-    vector<int> v;
-    for (int i = 1; i <= N; i++) {
-        v.push_back(i);
-    }
-
-    vector<string> temp{};
-    while (next_permutation(v.begin(), v.end())) {
-        string newItem{};
-        for (auto itemChar : v) {
-            newItem.push_back(itemChar + '0');
-        }        
-        temp.push_back(newItem + "\r\n");
-    }
-
-    string s = accumulate(temp.begin(), temp.end(), string{});
-	Solution60::Permutation_Sequence useClass;
-	Solution60::Permutation_Sequence::Permutation_Sequence_Model getTestModel = useClass.GetTestData001();
-    auto result4 = useClass.getPermutation(5, 23);
-	auto result1 = useClass.getPermutation(getTestModel.n, getTestModel.k);
+	Solution61::Rotate_List useClass;
+	Solution61::Rotate_List::Rotate_List_Model getTestModel = useClass.GetTestData001();
+	auto result1 = useClass.rotateRight(getTestModel.head, getTestModel.k);
 
 	getTestModel = useClass.GetTestData002();
-	auto result2 = useClass.getPermutation(getTestModel.n, getTestModel.k);
-
-	getTestModel = useClass.GetTestData003();
-	auto result3 = useClass.getPermutation(getTestModel.n, getTestModel.k);
-
+	auto result2 = useClass.rotateRight(getTestModel.head, getTestModel.k);
 	
+	getTestModel = useClass.GetTestData003();
+	auto result3 = useClass.rotateRight(getTestModel.head, getTestModel.k);
+
+	getTestModel = useClass.GetTestData004();
+	auto result4 = useClass.rotateRight(getTestModel.head, getTestModel.k);
+
+	getTestModel = useClass.GetTestData005();
+	auto result5 = useClass.rotateRight(getTestModel.head, getTestModel.k);
+	return 0;
 }
