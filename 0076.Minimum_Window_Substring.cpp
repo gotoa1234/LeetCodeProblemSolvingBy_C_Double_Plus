2c651d6404de519df46be8af80d4a2f89b12de7a
gotoa1234 <cap8825@gmail.com>
2022-12-26 23:26:22 +0800

    1. finish 0076

--- 0076.Minimum_Window_Substring.cpp
+++ 0076.Minimum_Window_Substring.cpp
@@ -45,50 +45,51 @@ namespace Solution76
 #pragma region Main
     public:
         /// <summary>
-        ///         思路 ：
-        ///      Runtime : 
-        /// Memory Usage : 
+        ///         思路 ：需要利用一個ascii的表紀錄所有字元出現的次數，並且利用滑動的方式更新leftIndex與rightIndex找出值
+        ///               此題有些複雜，可直接看代碼中的步驟重現
+        ///      Runtime :   7 ms Beats 97.85 %
+        /// Memory Usage : 7.8 MB Beats 93.94 %
         /// </summary>
         /// <returns></returns>
         string minWindow(string s, string t) 
         {
-            int asciiRecordTable[128] = { 0 };
-            int start = 0;
-
+            //※A=65 z=122 so 122 - 65 = 57+1 需要58的空間放字母
+            int asciiRecordTable[58] = { 0 };
             //1. Ascii 紀錄
             int useCount = 0;
             for (int index = 0; index < t.size(); index++) 
             {
-                if (asciiRecordTable[t[index]] == 0) 
+                if (asciiRecordTable[t[index] - 65] == 0)
                     useCount++;
-                asciiRecordTable[t[index]]++;
+                asciiRecordTable[t[index] - 65]++;
             }
-            int finalIndex = INT_MAX;
-            for(int startIndex = 0, endIndex = 0; endIndex < s.size(); endIndex++)
+            
+            //2. 滑框 - 一定是先找出符合t字串的所有字元
+            int targetEbdIndex = INT_MAX;
+            int targetStartIndex = 0;
+            for(int leftIndex = 0, rightIndex = 0; rightIndex < s.size(); rightIndex++)
             {
-                asciiRecordTable[s[endIndex]]--;
-                if (asciiRecordTable[s[endIndex]] == 0) {
-                    useCount--;
+                //2-1. 直到useCount = 0 表示所有的t字元都出現了
+                asciiRecordTable[s[rightIndex] - 65]--;
+                if (asciiRecordTable[s[rightIndex] - 65] == 0) {
+                    useCount--;//每找到1個減1
                 }
-                if (useCount == 0) 
-                {
-                    while (useCount == 0) 
-                    {
-                        if (finalIndex > endIndex - startIndex + 1) 
-                        {
-                            finalIndex = min(finalIndex, endIndex - startIndex + 1);
-                            start = startIndex;
-                        }
-                        asciiRecordTable[s[startIndex]]++;
-                        if (asciiRecordTable[s[startIndex]] > 0)
-                            useCount++;
-                        
-                        startIndex++;
-                    }
-                }
+                //2-2. 當找到完畢後，進行滑框
+				while (useCount == 0)
+				{
+					if (targetEbdIndex > rightIndex - leftIndex + 1)
+					{
+                        //3. 當有更小的範圍字串，則更新結果值
+						targetEbdIndex = min(targetEbdIndex, rightIndex - leftIndex + 1);
+						targetStartIndex = leftIndex;
+					}
+					asciiRecordTable[s[leftIndex] - 65]++;
+					if (asciiRecordTable[s[leftIndex] - 65] > 0)
+						useCount++;//useCount大於0時表示出現新的t字元，要跳出2-2進行滑動
+					leftIndex++;
+				}
             }
-
-            return finalIndex == INT_MAX ? "" : s.substr(start, finalIndex);
+            return targetEbdIndex == INT_MAX ? "" : s.substr(targetStartIndex, targetEbdIndex);
         }
     public:
 #pragma endregion Main
