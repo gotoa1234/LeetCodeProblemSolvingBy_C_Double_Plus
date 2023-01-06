d0184fc425a7e396480057181933e10780208f90
gotoa1234 <cap8825@gmail.com>
2023-01-06 16:30:20 +0800

    1. finish 0085

--- Hard/0085.Maximal_Rectangle.cpp
+++ Hard/0085.Maximal_Rectangle.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <unordered_map>
 #include <string>
+#include <stack>
 using namespace std;
 
 namespace Solution85
@@ -8,7 +9,7 @@ namespace Solution85
 #pragma region Paste to execute 
     /*
     #include <unordered_map>
-    #include "0085.Maximal_Rectangle.cpp"
+    #include "Hard/0085.Maximal_Rectangle.cpp"
     using namespace Solution85;
 
     int main()
@@ -42,13 +43,49 @@ namespace Solution85
 #pragma region Main
     public:
         /// <summary>
-        ///         思路 ：
-        ///      Runtime : 
-        /// Memory Usage : 
+        ///         思路 ：參考 0084的計算方式，但每一層Y軸視為1個面積的容積，往下層時需再累計前一層的值
+        ///                若本層的值為0則歸0
+        ///      Runtime :   51 ms Beats 79.23 %
+        /// Memory Usage : 13.1 MB Beats 77.83 %
         /// </summary>
         /// <returns></returns>
         int maximalRectangle(vector<vector<char>>& matrix) {
-            return {};
+            
+            if (matrix.empty() || matrix[0].empty()) 
+                return 0;
+            int result = 0;
+            int yAxisMax = matrix.size();
+            int xAxisMax = matrix[0].size();
+            int useDistance = 0;
+            vector<int> height(xAxisMax + 1);
+            //1. 每層計算
+            for (int yAxis = 0; yAxis < yAxisMax; yAxis++) 
+            {
+                stack<int> stackContainer;
+                for (int xAxis = 0; xAxis < xAxisMax + 1; xAxis++)
+                {
+                    //2. 跳過最後一個元素，最後一個元素是添加的
+                    if (xAxis < xAxisMax)
+                    {
+                        height[xAxis] = matrix[yAxis][xAxis] == '1' 
+                                       ? height[xAxis] + 1 
+                                       : 0;
+                    }
+                    //3. 同0084的計算方式
+                    while (!stackContainer.empty() && 
+                           height[stackContainer.top()] >= height[xAxis]
+                        ) 
+                    {
+                        int cur = stackContainer.top(); 
+                        stackContainer.pop();
+                        int useDistance = stackContainer.empty() ? xAxis 
+                                                    : (xAxis - stackContainer.top() - 1);
+                        result = max(result, height[cur] * useDistance );
+                    }
+                    stackContainer.push(xAxis);
+                }
+            }
+            return result;
         }
 
     public:
@@ -66,7 +103,7 @@ namespace Solution85
                               {'1', '0', '1', '1', '1'}, 
                               {'1', '1', '1', '1', '1'}, 
                               {'1', '0', '0', '1', '0'} };
-            return result;//expect: 0
+            return result;//expect: 6
         };
         /// <summary>
         /// test 2
--- LeetCodeProblemSolvingBy_C_Double_Plus.cpp
+++ LeetCodeProblemSolvingBy_C_Double_Plus.cpp
@@ -1,15 +1,15 @@
 ﻿#include <unordered_map>
-#include "Hard\0084.Largest_Rectangle_in_Histogram.cpp"
-using namespace Solution84;
+#include "Hard/0085.Maximal_Rectangle.cpp"
+using namespace Solution85;
 
 int main()
 {
-    Solution84::Largest_Rectangle_in_Histogram useClass;
-    Solution84::Largest_Rectangle_in_Histogram::Largest_Rectangle_in_Histogram_Model getTestModel = useClass.GetTestData001();
-    auto result1 = useClass.largestRectangleArea(getTestModel.heights);
+    Solution85::Maximal_Rectangle useClass;
+    Solution85::Maximal_Rectangle::Maximal_Rectangle_Model getTestModel = useClass.GetTestData001();
+    auto result1 = useClass.maximalRectangle(getTestModel.matrix);
 
     getTestModel = useClass.GetTestData002();
-    auto result2 = useClass.largestRectangleArea(getTestModel.heights);
+    auto result2 = useClass.maximalRectangle(getTestModel.matrix);
 
     return 0;
 }
