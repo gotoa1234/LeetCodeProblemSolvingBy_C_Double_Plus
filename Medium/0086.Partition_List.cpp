d8b61d19fb1a5d786657d42d5e73404b1122a861
gotoa1234 <cap8825@gmail.com>
2023-01-10 11:03:04 +0800

    1. finish 0086

--- Medium/0086.Partition_List.cpp
+++ Medium/0086.Partition_List.cpp
@@ -8,7 +8,7 @@ namespace Solution86
 #pragma region Paste to execute 
     /*
     #include <unordered_map>
-    #include "0086.Partition_List.cpp"
+    #include "Medium/0086.Partition_List.cpp"
     using namespace Solution86;
 
     int main()
@@ -52,14 +52,37 @@ namespace Solution86
 #pragma region Main
     public:
         /// <summary>
-        ///         思路 ：
-        ///      Runtime : 
-        /// Memory Usage : 
+        ///         思路 ：利用兩個節點，一個紀錄小於X的，另一個紀錄大於x的，最後將小的節點與大的節點串起來
+        ///      Runtime :    3 ms Beats 96.56 %
+        /// Memory Usage : 10.2 MB Beats 74.99 %
         /// </summary>
         /// <returns></returns>
     public:
         ListNode* partition(ListNode* head, int x) {
-            return {};
+            ListNode* minList = new ListNode(0);
+            ListNode* bigList = new ListNode(0);
+            ListNode* minMoveNode = minList;
+            ListNode* bigMoveNode = bigList;
+            //1. 遍歷傳進的head節點
+            while (head)
+            {
+                //1-1. 小於x放進 minList中
+                if (head->val < x)
+                {
+                    minMoveNode->next = head;
+                    minMoveNode = minMoveNode->next;
+                }
+                else//1-2. 大於等於x放進 bigList中
+                {
+                    bigMoveNode->next = head;
+                    bigMoveNode = bigMoveNode->next;
+                }
+                head = head->next;
+            }
+            //將小 + 大 鏈結串鍊合併
+            minMoveNode->next = bigList->next;
+            bigMoveNode->next = nullptr;
+            return minList->next;//回傳
         }
 
     public:
