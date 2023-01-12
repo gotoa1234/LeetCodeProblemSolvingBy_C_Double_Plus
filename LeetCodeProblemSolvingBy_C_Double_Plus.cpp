a0d9f92f232471cf955c8cf7de3181072afeacb3
gotoa1234 <cap8825@gmail.com>
2023-01-12 18:18:24 +0800

    1. template

--- LeetCodeProblemSolvingBy_C_Double_Plus.cpp
+++ LeetCodeProblemSolvingBy_C_Double_Plus.cpp
@@ -1,17 +1,14 @@
 ﻿#include <unordered_map>
-#include "Medium/0089.Gray_Code.cpp"
-using namespace Solution89;
+#include "Medium\0090.Subsets_II.cpp"
+using namespace Solution90;
 
 int main()
 {
-    Solution89::Gray_Code useClass;
-    Solution89::Gray_Code::Gray_Code_Model getTestModel = useClass.GetTestData001();
-    auto result1 = useClass.grayCode(getTestModel.n);
+    Solution90::Subsets_II useClass;
+    Solution90::Subsets_II::Subsets_II_Model getTestModel = useClass.GetTestData001();
+    auto result1 = useClass.Subsets_IIWithDup(getTestModel.nums);
 
     getTestModel = useClass.GetTestData002();
-    auto result2 = useClass.grayCode(getTestModel.n);
-
-    getTestModel = useClass.GetTestData003();
-    auto result3 = useClass.grayCode(getTestModel.n);
+    auto result2 = useClass.Subsets_IIWithDup(getTestModel.nums);
     return 0;
 }
--- Medium/0090.Subsets_II.cpp
+++ Medium/0090.Subsets_II.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <unordered_map>
 #include <string>
+#include <algorithm>
 using namespace std;
 
 namespace Solution90
@@ -8,17 +9,17 @@ namespace Solution90
 #pragma region Paste to execute 
     /*
     #include <unordered_map>
-    #include "0090.Subsets.cpp"
+    #include "Medium\0090.Subsets_II.cpp"
     using namespace Solution90;
 
     int main()
     {
-        Solution90::Subsets useClass;
-        Solution90::Subsets::Subsets_Model getTestModel = useClass.GetTestData001();
-        auto result1 = useClass.subsetsWithDup(getTestModel.nums);
+        Solution90::Subsets_II useClass;
+        Solution90::Subsets_II::Subsets_II_Model getTestModel = useClass.GetTestData001();
+        auto result1 = useClass.Subsets_IIWithDup(getTestModel.nums);
 
         getTestModel = useClass.GetTestData002();
-        auto result2 = useClass.subsetsWithDup(getTestModel.nums);
+        auto result2 = useClass.Subsets_IIWithDup(getTestModel.nums);
         return 0;
     }
     */
@@ -27,11 +28,11 @@ namespace Solution90
     /// <summary>
     /// ¤l¶° II
     /// </summary>
-    class Subsets
+    class Subsets_II
     {
 #pragma region Model
     public:
-        class Subsets_Model
+        class Subsets_II_Model
         {
         public:
             vector<int> nums;
@@ -46,9 +47,52 @@ namespace Solution90
         /// Memory Usage : 
         /// </summary>
         /// <returns></returns>
-        vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-            return {};
+        vector<vector<int>> Subsets_IIWithDup(vector<int>& nums) {
+            vector<vector<int>> result{};
+            vector<int> compose{};
+            int currentLength = 0;
+            sort(nums.begin(), nums.end());
+            for (int index = 0; index < nums.size(); index++)
+            {
+                if (!compose.empty() && compose[0] == nums[index])
+                {
+                    compose.push_back(nums[index]);
+                }
+                else
+                {
+                    compose.clear();
+                    compose.push_back(nums[index]);
+                }
+                currentLength = result.size();
+                result.push_back(compose);
+
+                for (int innerIndex = 0; innerIndex < currentLength; innerIndex++)
+                {
+                    vector<int> container = result[innerIndex];
+                    container.push_back(nums[index]);
+                    result.push_back(container);
+                }
+            }
+            result.insert(result.begin(), vector<int>());
+            return result;
         }
+
+        //void findSubsets(int ind, vector < int >& nums, vector < int >& ds, vector < vector < int >>& ans) {
+        //    ans.push_back(ds);
+        //    for (int i = ind; i < nums.size(); i++) {
+        //        if (i != ind && nums[i] == nums[i - 1]) continue;
+        //        ds.push_back(nums[i]);
+        //        findSubsets(i + 1, nums, ds, ans);
+        //        ds.pop_back();
+        //    }
+        //}
+        //vector < vector < int >> subsetsWithDup(vector < int >& nums) {
+        //    vector < vector < int >> ans;
+        //    vector < int > ds;
+        //    sort(nums.begin(), nums.end());
+        //    findSubsets(0, nums, ds, ans);
+        //    return ans;
+        //}
     public:
 #pragma endregion Main
 
@@ -57,9 +101,9 @@ namespace Solution90
         /// <summary>
         /// test 1
         /// </summary>        
-        Subsets_Model GetTestData001(void)
+        Subsets_II_Model GetTestData001(void)
         {
-            Subsets_Model result;
+            Subsets_II_Model result;
             result.nums = { 1,2,2 };
             return result;//expect:  [[],[1],[1,2],[1,2,2],[2],[2,2]]
         };
@@ -67,9 +111,9 @@ namespace Solution90
         /// <summary>
         /// test 2
         /// </summary>   
-        Subsets_Model GetTestData002(void)
+        Subsets_II_Model GetTestData002(void)
         {
-            Subsets_Model result;
+            Subsets_II_Model result;
             result.nums = { 0 };
             return result;//expect:[[], [0]]
         };
