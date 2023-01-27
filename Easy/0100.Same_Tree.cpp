3955c6e53134f23166af362524d51088a5c03c93
gotoa1234 <cap8825@gmail.com>
2023-01-27 16:21:53 +0800

    1. finish 0100.Same_Tree.cpp

--- Easy/0100.Same_Tree.cpp
+++ Easy/0100.Same_Tree.cpp
@@ -6,19 +6,24 @@ namespace Solution100
 {
 #pragma region Paste to execute 
 	/*
-	#include "Easy/0100.Recover_Binary_Search_Tree.cpp"
-	using namespace Solution100;
-	using namespace std;
-
-	Solution100::Recover_Binary_Search_Tree useClass;
-	Solution100::Recover_Binary_Search_Tree::Recover_Binary_Search_Tree_Model getTestModel = useClass.GetTestData001();
-	auto result1 = useClass.inorderTraversal(getTestModel.root);
-
-	getTestModel = useClass.GetTestData002();
-	auto result2 = useClass.inorderTraversal(getTestModel.root);
-
-	getTestModel = useClass.GetTestData003();
-	auto result3 = useClass.inorderTraversal(getTestModel.root);
+    #include "Easy\0100.Same_Tree.cpp"
+    using namespace Solution100;
+    using namespace std;
+    
+    
+    int main()
+    {
+    	Solution100::Recover_Binary_Search_Tree useClass;
+    	Solution100::Recover_Binary_Search_Tree::Recover_Binary_Search_Tree_Model getTestModel = useClass.GetTestData001();
+    	auto result1 = useClass.isSameTree(getTestModel.p, getTestModel.q);
+    
+    	getTestModel = useClass.GetTestData002();
+    	auto result2 = useClass.isSameTree(getTestModel.p, getTestModel.q);
+    
+    	getTestModel = useClass.GetTestData003();
+    	auto result3 = useClass.isSameTree(getTestModel.p, getTestModel.q);
+    
+    }
 	*/
 #pragma endregion Paste to execute
 
@@ -50,12 +55,30 @@ namespace Solution100
 #pragma region Main
 	public:
 		/// <summary>
-		///          思路： 
-		///       Runtime: 
-		///  Memory Usage :
+		///          思路：利用遞迴比對兩個節點結構與值是否相同，不相同返回false
+		///       Runtime:  0 ms Beats 100 %
+		///  Memory Usage :10 MB Beats 51 %
 		/// </summary>
-	    bool isSameTree(TreeNode* p, TreeNode* q) {
-         return {};
+	    bool isSameTree(TreeNode* p, TreeNode* q) 
+		{
+			//1. 節點結構相同返回true
+			if (p == NULL && q == NULL)
+				return true;
+			//2. 節點結構不相同返回false
+			if ((p != NULL && q == NULL) ||
+				p == NULL && q != NULL
+				)
+				return false;
+
+			//3. 檢查下一節點結構/值是否相同
+			bool leftNode = isSameTree(p->left, q->left);
+			bool right = isSameTree(p->right, q->right);
+			bool check = p->val == q->val;
+			//4. 相同返回true
+			if (leftNode && right && check)
+				return true;
+			//5. 不相同返回false
+			return false;
         }
 	
 #pragma endregion Main
--- LeetCodeProblemSolvingBy_C_Double_Plus.cpp
+++ LeetCodeProblemSolvingBy_C_Double_Plus.cpp
@@ -1,15 +1,18 @@
-﻿#include "Medium\0099.Recover_Binary_Search_Tree.cpp"
-using namespace Solution99;
+﻿#include "Easy\0100.Same_Tree.cpp"
+using namespace Solution100;
 using namespace std;
+
 
 int main()
 {
-
-	Solution99::Recover_Binary_Search_Tree useClass;
-	Solution99::Recover_Binary_Search_Tree::Recover_Binary_Search_Tree_Model getTestModel = useClass.GetTestData001();
-	useClass.recoverTree(getTestModel.root);
+	Solution100::Recover_Binary_Search_Tree useClass;
+	Solution100::Recover_Binary_Search_Tree::Recover_Binary_Search_Tree_Model getTestModel = useClass.GetTestData001();
+	auto result1 = useClass.isSameTree(getTestModel.p, getTestModel.q);
 
 	getTestModel = useClass.GetTestData002();
-	useClass.recoverTree(getTestModel.root);
+	auto result2 = useClass.isSameTree(getTestModel.p, getTestModel.q);
+
+	getTestModel = useClass.GetTestData003();
+	auto result3 = useClass.isSameTree(getTestModel.p, getTestModel.q);
 
 }
