772511ad0a415e419cdf423cd7e61265d8140262
gotoa1234 <cap8825@gmail.com>
2023-02-24 14:22:41 +0800

    1. finish 0129

--- LeetCodeProblemSolvingBy_C_Double_Plus.cpp
+++ LeetCodeProblemSolvingBy_C_Double_Plus.cpp
@@ -1,16 +1,14 @@
 ﻿#include <unordered_map>
-#include "Medium/0128.Longest_Consecutive_Sequence.cpp"
-using namespace Solution128;
+#include "Medium/0129.Sum_Root_to_Leaf_Numbers.cpp"
+using namespace Solution129;
 
 int main()
 {
-    Solution128::Longest_Consecutive_Sequence useClass;
-    Solution128::Longest_Consecutive_Sequence::Longest_Consecutive_Sequence_Model getTestModel = useClass.GetTestData001();
-    auto result1 = useClass.longestConsecutive(getTestModel.nums);
+    Solution129::Sum_Root_to_Leaf_Numbers useClass;
+    Solution129::Sum_Root_to_Leaf_Numbers::Sum_Root_to_Leaf_Numbers_Model getTestModel = useClass.GetTestData001();
+    auto result1 = useClass.sumNumbers(getTestModel.root);
 
     getTestModel = useClass.GetTestData002();
-    auto result2 = useClass.longestConsecutive(getTestModel.nums);
-
-
+    auto result2 = useClass.sumNumbers(getTestModel.root);
     return 0;
 }
--- Medium/0129.Sum_Root_to_Leaf_Numbers.cpp
+++ Medium/0129.Sum_Root_to_Leaf_Numbers.cpp
@@ -48,20 +48,34 @@ namespace Solution129
         class Sum_Root_to_Leaf_Numbers_Model
         {
         public:
-            vector<int> nums;
+            TreeNode* root;
         };
 #pragma endregion Model
 
 #pragma region Main
     public:
         /// <summary>
-        ///         思路 ：
-        ///      Runtime : 
-        /// Memory Usage : 
+        ///         思路 ：利用遞迴將每次的值都乘以10累加，最後返還
+        ///      Runtime :   0 ms Beats 100 %
+        /// Memory Usage : 9.2 MB Beats 87.77 %
         /// </summary>
         /// <returns></returns>
-        int sumNumbers(TreeNode* root) {
-            return{};
+        int sumNumbers(TreeNode* root) 
+        {            
+            return FindRoot(root, 0);
+        }
+
+        int FindRoot(TreeNode* root, int price)
+        {                                   
+            //1. 每次進來都拿現有的值 * 10 累加
+            price = root->val + price * 10;
+            if (root->left && root->right)//2-1. 兩個節點的情況  
+                return FindRoot(root->left, price) + FindRoot(root->right, price);
+            else if(root->left && root->right == nullptr)//2-2. 左節點的情況      
+                return FindRoot(root->left, price);            
+            else if(root->left == nullptr && root->right)//2-3. 右節點的情況
+                return FindRoot(root->right, price);            
+            return price;//2-4. 走到底了，直接回傳
         }
 
 
@@ -76,7 +90,9 @@ namespace Solution129
         Sum_Root_to_Leaf_Numbers_Model GetTestData001(void)
         {
             Sum_Root_to_Leaf_Numbers_Model result;
-            result.nums = { 1, 2, 3 };
+            result.root = new TreeNode(1);
+            result.root->left = new TreeNode(2); 
+            result.root->right = new TreeNode(3);
             return result;//expect: 25
         };
 
@@ -86,7 +102,12 @@ namespace Solution129
         Sum_Root_to_Leaf_Numbers_Model GetTestData002(void)
         {
             Sum_Root_to_Leaf_Numbers_Model result;
-            result.nums = { 4,9,0,5,1 };
+            result.root = new TreeNode(4);
+            result.root->left = new TreeNode(9);
+            result.root->right = new TreeNode(0);
+
+            result.root->left->left = new TreeNode(5);
+            result.root->left->right = new TreeNode(1);
             return result;//expect: 1026
         };
 
