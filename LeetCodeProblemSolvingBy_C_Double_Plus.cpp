1a50391be92ccc78fcca6247f4dfce01265f2261
gotoa1234 <cap8825@gmail.com>
2023-03-05 23:23:34 +0800

    1. finish 0117

--- LeetCodeProblemSolvingBy_C_Double_Plus.cpp
+++ LeetCodeProblemSolvingBy_C_Double_Plus.cpp
@@ -1,16 +1,13 @@
-﻿#include "Medium\0134.Gas_Station.cpp"
-using namespace Solution134;
+﻿#include "Medium\0117.Populating_Next_Right_Pointers_in_Each_Node_II.cpp"
+using namespace Solution117;
 using namespace std;
 
 int main()
 {
-	Solution134::Gas_Station useClass;
-	Solution134::Gas_Station::Gas_Station_Model getTestModel = useClass.GetTestData001();
-	auto result1 = useClass.canCompleteCircuit(getTestModel.gas, getTestModel.cost);
+    Solution117::Populating_Next_Right_Pointers_in_Each_Node_II useClass;
+    Solution117::Populating_Next_Right_Pointers_in_Each_Node_II::Populating_Next_Right_Pointers_in_Each_Node_II_Model getTestModel = useClass.GetTestData001();
+    auto result1 = useClass.connect(getTestModel.root);
 
-	getTestModel = useClass.GetTestData002();
-	auto result2 = useClass.canCompleteCircuit(getTestModel.gas, getTestModel.cost);
-
-	getTestModel = useClass.GetTestData003();
-	auto result3 = useClass.canCompleteCircuit(getTestModel.gas, getTestModel.cost);
+    getTestModel = useClass.GetTestData002();
+    auto result2 = useClass.connect(getTestModel.root);
 }
--- Medium/0117.Populating_Next_Right_Pointers_in_Each_Node_II.cpp
+++ Medium/0117.Populating_Next_Right_Pointers_in_Each_Node_II.cpp
@@ -9,12 +9,13 @@ namespace Solution117
     #include "Medium\0117.Populating_Next_Right_Pointers_in_Each_Node_II.cpp"
     using namespace Solution117;
     using namespace std;
+    
     int main()
     {
-        Solution113::Path_Sum useClass;
-        Solution113::Path_Sum::Populating_Next_Right_Pointers_in_Each_Node_II_Model getTestModel = useClass.GetTestData001();
+        Solution117::Populating_Next_Right_Pointers_in_Each_Node_II useClass;
+        Solution117::Populating_Next_Right_Pointers_in_Each_Node_II::Populating_Next_Right_Pointers_in_Each_Node_II_Model getTestModel = useClass.GetTestData001();
         auto result1 = useClass.connect(getTestModel.root);
-
+    
         getTestModel = useClass.GetTestData002();
         auto result2 = useClass.connect(getTestModel.root);
     }
@@ -53,12 +54,37 @@ namespace Solution117
 #pragma region Main
     public:
         /// <summary>
-        ///         思路 ：
-        ///      Runtime : 
-        /// Memory Usage : 
-
+        ///         思路 ：BFS 廣度優先，遞迴版本是只做根節點然後每個的左節點為起始點，不斷往下
+        ///               當前節點會處理下一層每個左節點的next指向右邊的節點，直到走到底後，將當前節點的左節點在持續重複直到走到底
+        ///           ※備註，題目要求常數空間(You may only use constant extra space.)
+        ///      Runtime :   12 ms Beats 82.20 %
+        /// Memory Usage : 17.4 MB Beats 72.64 %
         Node* connect(Node* root) {
-            return {};
+            //1. 跳出條件，處理完成
+            if (nullptr == root)   
+                return nullptr;
+
+            //2. 設定一個暫存當前節點的左節點用的Node
+            Node dummy(0);
+            //3-1. 每次將自己的節點指向右邊(next)，做平移
+            for (Node* currentNode = root, *lastNode = &dummy; currentNode; currentNode = currentNode->next)
+            {
+                //3-2. 將當前左節點給上一個右節點->next指向
+                if (currentNode->left)
+                {
+                    lastNode->next = currentNode->left;
+                    lastNode = lastNode->next;
+                }//3-3. 將當前右節點給上一個左節點->next指向 
+                if (currentNode->right)
+                {
+                    lastNode->next = currentNode->right;
+                    lastNode = lastNode->next;
+                }
+            }
+            //4. 該層都已完成，進入下一層
+            connect(dummy.next);
+
+            return root;
         }
 #pragma endregion Main
 
