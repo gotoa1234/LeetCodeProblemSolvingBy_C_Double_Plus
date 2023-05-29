61c8e9c46ea2553c9047b9c8ccab2727683ae162
gotoa1234 <cap8825@gmail.com>
2023-05-29 11:58:24 +0800

    1. finish 0152

--- LeetCodeProblemSolvingBy_C_Double_Plus.cpp
+++ LeetCodeProblemSolvingBy_C_Double_Plus.cpp
@@ -1,17 +1,14 @@
 ﻿#include <unordered_map>
-#include "Medium/0151.Reverse_Words_in_a_String.cpp"
-using namespace Solution151;
+#include "Medium/0152.Maximum_Product_Subarray.cpp"
+using namespace Solution152;
 
 int main()
 {
-    Solution151::Reverse_Words_in_a_String useClass;
-    Solution151::Reverse_Words_in_a_String::Reverse_Words_in_a_String_Model getTestModel = useClass.GetTestData001();
-    auto result1 = useClass.reverseWords(getTestModel.s);
+    Solution152::Maximum_Product_Subarray useClass;
+    Solution152::Maximum_Product_Subarray::Maximum_Product_Subarray_Model getTestModel = useClass.GetTestData001();
+    auto result1 = useClass.maxProduct(getTestModel.nums);
 
     getTestModel = useClass.GetTestData002();
-    auto result2 = useClass.reverseWords(getTestModel.s);
-
-    getTestModel = useClass.GetTestData003();
-    auto result3 = useClass.reverseWords(getTestModel.s);
+    auto result2 = useClass.maxProduct(getTestModel.nums);
     return 0;
 }
--- Medium/0152.Maximum_Product_Subarray.cpp
+++ Medium/0152.Maximum_Product_Subarray.cpp
@@ -7,8 +7,7 @@ using namespace std;
 namespace Solution152
 {
 #pragma region Paste to execute 
-    /*
-    #include <unordered_map>
+    /*    #include <unordered_map>
     #include "Medium/0152.Maximum_Product_Subarray.cpp"
     using namespace Solution152;
 
@@ -22,6 +21,7 @@ namespace Solution152
         auto result2 = useClass.maxProduct(getTestModel.nums);
         return 0;
     }
+
     */
 #pragma endregion Paste to executes
 
@@ -42,14 +42,35 @@ namespace Solution152
 #pragma region Main
     public:
         /// <summary>
-        ///         思路： 
-        ///      Runtime： 
-        /// Memory Usage： 
+        ///         思路： 子陣列乘積具有連續性，因此可以用兩次For回圈，由前往後與由後往前
+        ///                找出連續乘積最大值即為結果。
+        ///      Runtime：    0 ms Beats 100 %
+        /// Memory Usage： 13.9 MB Beats 35.6 %
         /// </summary>
         /// <returns></returns>
     public:
         int maxProduct(vector<int>& nums) {
-            return {  };
+            int result = nums[0];
+            int caculation = 1;
+            //1. 正向搜尋
+            for (int index = 0; index < nums.size(); index++)
+            {
+                caculation *= nums[index];
+                result = max(result, caculation);
+                //遇到0時需要重置為1
+                if (nums[index] == 0)
+                    caculation = 1;
+            }
+            //2. 反向搜尋
+            caculation = 1;
+			for (int index = nums.size() - 1; index >= 0; index--)
+            {
+                caculation *= nums[index];
+                result = max(result, caculation);
+                if (nums[index] == 0)
+                    caculation = 1;
+            }
+            return result;
         }
 
     public:
