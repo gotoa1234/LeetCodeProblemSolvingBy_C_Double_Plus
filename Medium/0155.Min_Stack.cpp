4bc253cb5096f5ed71068d298d4b53a3697fefb7
gotoa1234 <cap8825@gmail.com>
2023-06-06 14:49:32 +0800

    1. finish 0155

--- Medium/0155.Min_Stack.cpp
+++ Medium/0155.Min_Stack.cpp
@@ -45,32 +45,43 @@ namespace Solution155
 #pragma region Main
     public:
         /// <summary>
-        ///         思路： 
-        ///      Runtime： 
-        /// Memory Usage： 
+        ///         思路： 設定兩個Stack 一個是紀錄原本的，另一個紀錄最小的，當有更小的時候才記錄到最小的Stack中        
+        ///      Runtime：   21 ms Beats 82.41 %
+        /// Memory Usage： 16.4 MB Beats 40.41 %
         /// </summary>
         /// <returns></returns>
     public:
         class MinStack {
+        private:
+            stack<int> _BaseStack;
+            stack<int> _MinStack;
         public:
             MinStack() {
 
             }
 
             void push(int val) {
-
+                _BaseStack.push(val);
+                if (_MinStack.empty() || val <= _MinStack.top())
+                {
+                    _MinStack.push(val);
+                }
             }
 
             void pop() {
-
+                if (_BaseStack.top() == _MinStack.top())
+                {
+                    _MinStack.pop();
+                }
+                _BaseStack.pop();
             }
 
             int top() {
-
+                return _BaseStack.top();
             }
 
             int getMin() {
-
+                return _MinStack.top();
             }
         };
     public:
