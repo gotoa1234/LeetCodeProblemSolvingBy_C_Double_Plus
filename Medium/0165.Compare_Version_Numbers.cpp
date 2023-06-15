04db24143615f504304d9d0b23a323cc77263704
gotoa1234 <cap8825@gmail.com>
2023-06-15 15:27:48 +0800

    1. finish 0165

--- Medium/0165.Compare_Version_Numbers.cpp
+++ Medium/0165.Compare_Version_Numbers.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <unordered_map>
 #include <string>
+#include <istringstream>
 using namespace std;
 
 namespace Solution165
@@ -42,15 +43,55 @@ namespace Solution165
 #pragma region Main
     public:
         /// <summary>
-        ///         思路：         
-        ///      Runtime：
-        /// Memory Usage：
+        ///         思路：在C++中比較麻煩沒有split分割字串，因此需要自行對`.`分割
+        ///               每次對V1與V2 `.`前的字串做整數值的比較，若有其中一個版本大就跳出
+        ///               否則一直比對，若最後都無法分出大小，表示相等回傳0
+        ///      Runtime：  0 ms Beats 100 %
+        /// Memory Usage： 6.2 MB Beats 68.31 %
         /// </summary>
         /// <returns></returns>
     public:
-        int compareVersion(string version1, string version2) {
-            return {};
+        int compareVersion(string version1, string version2) 
+        {
+            string v1CompareString;
+            string v2CompareString;
+            int v1MoveIndex = 0;
+            int v2MoveIndex = 0;
+            //1. 比較兩個版本
+            for (int v1Index = 0, v2Index = 0;
+                v1Index < version1.size() || v2Index < version2.size();
+				v1Index++, v2Index++)
+            {
+                //2-1. 取得版本1 在.以前的資料
+                while (v1Index < version1.size() && version1[v1Index] != '.')
+                {
+                    v1CompareString.push_back(version1[v1Index]);
+                    v1Index++;
+                }
+                
+                //2-2. 取得版本2 在.以前的資料
+                while (v2Index < version2.size() && version2[v2Index] != '.')
+                {
+                    v2CompareString.push_back(version2[v2Index]);
+                    v2Index++;
+                }
+                
+                //3. 轉成整數並且比較
+                auto v1RealVersion = atoi(v1CompareString.c_str());
+                auto v2RealVersion = atoi(v2CompareString.c_str());
+                if (v1RealVersion > v2RealVersion)
+                    return 1;
+                else if (v1RealVersion < v2RealVersion)
+                    return -1;
+                //4. 表示要往下一個.前的數值比較，這次的暫存資料清空
+                v1CompareString.clear();
+                v2CompareString.clear();
+            }
+            //5. 如果Version1 與 Version2 都比較完，表示只有平手(0)結果
+            return 0;
         }
+
+
     public:
 #pragma endregion Main
 
@@ -73,8 +114,8 @@ namespace Solution165
         Compare_Version_Numbers_Model GetTestData002(void)
         {
             Compare_Version_Numbers_Model result;
-            result.version1 = "1.0";
-            result.version2 = "1.0.0";
+            result.version1 = "001.0";
+            result.version2 = "00101.0.0";
             return result;//expect: 0           
         };
 
