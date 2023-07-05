6043535b856369acaa2e63135978caaefce2d797
gotoa1234 <cap8825@gmail.com>
2023-07-05 09:47:20 +0800

    1. 補上演算過程

--- Hard/0174.Dungeon_Game.cpp
+++ Hard/0174.Dungeon_Game.cpp
@@ -40,7 +40,7 @@ namespace Solution174
 #pragma region Main
     public:
         /// <summary>
-        ///         思路 ：優化版本
+        ///         思路 ：優化版本，先將周圍一圈計算出來，然後取出大於0的最小值
         ///      Runtime :   4 ms Beats 91.33 %
         /// Memory Usage : 8.8 MB Beats 87.29 %
         /// </summary>
@@ -51,19 +51,23 @@ namespace Solution174
             vector<vector<int>> dp(rowMax, vector<int>(colMax , 0));                        
             dp[rowMax - 1][colMax - 1] = min(0, dungeon[rowMax - 1][colMax - 1]);
 
+            //1. 算出row最邊的值
             for (int row = rowMax - 2; row >= 0; row--) {
                 dp[row][colMax - 1] = min(dungeon[row][colMax - 1] + dp[row + 1][colMax - 1], 0);
             }
 
+            //2. 算出col最邊的值
             for (int col = colMax - 2; col >= 0; col--) {
                 dp[rowMax - 1][col] = min(dungeon[rowMax - 1][col] + dp[rowMax - 1][col + 1], 0);
             }
           
+            //3. 取出大於0的最小值
             for (int row = rowMax - 2; row >= 0; row--) {
                 for (int cow = colMax - 2; cow >= 0; cow--) {
-                    dp[row][cow] = min(dungeon[row][cow] + max(dp[row + 1][cow], dp[row][cow + 1]), 0);
+                    dp[row][cow] = min(dungeon[row][cow] + max(dp[row + 1][cow], dp[row][cow + 1]), 0);                                   
                 }
             }
+            //4. 最後取絕對值 +1 = 距離最小生命的所需點數
             return 1 + abs(dp[0][0]);
         }
 
@@ -71,6 +75,11 @@ namespace Solution174
         /// <summary>
         /// 本題基本的解法，利用動態規劃，由迷宮的最後倒推回起點
         /// </summary>
+        ///     (範例1)演進圖  : 最後的答案在[0][0] 得到7 
+        ///               * * *    * *  *    * *  *    * *  *    *  *  *    *  *  *     * * 2      *  5  2    7  5  2
+        ///               * * * -> * *  * -> * *  * -> * *  5 -> * 11  5 -> 6 11  5 ->  6 11  5 -> 6 11  5 -> 6 11  5
+        ///               * * -5   * 1 -5    1 1 -5    1 1 -5    1  1 -5    1  1 -5     1  1 -5    1  1 -5    1  1 -5
+        ///               
         ///      Runtime :   8 ms Beats 64.88 %
         /// Memory Usage : 8.8 MB Beats 72.47 %
         /// <returns></returns>
