5a88f5416230564752e2f2769c91fe8c9d67871a
gotoa1234 <cap8825@gmail.com>
2023-07-14 18:17:52 +0800

    1. 先補上O(n) 方案

--- Medium/0209.Minimum_Size_Subarray_Sum.cpp
+++ Medium/0209.Minimum_Size_Subarray_Sum.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <unordered_map>
+#include <algorithm>
 using namespace std;
 
 namespace Solution209
@@ -8,7 +9,7 @@ namespace Solution209
 #pragma region Paste to execute 
     /*
     #include <vector>
-    #include "0209.Minimum_Size_Subarray_Sum.cpp"
+    #include "Medium\0209.Minimum_Size_Subarray_Sum.cpp"
     using namespace Solution209;
 
     int main()
@@ -46,10 +47,27 @@ namespace Solution209
     public:
         ///         «ä¸ô :         
         ///      Runtime:  
-        /// Memory Usage : 
-        int minSubArrayLen(int target, vector<int>& nums) {
-            return {};
+        /// Memory Usage :         
+        int minSubArrayLen(int target, vector<int>& nums) 
+        {
+            int leftIndex = 0;
+            int rightIndex = 0;
+            int resultCount = INT_MAX;
+            int reminder = 0;
+            while (rightIndex < nums.size()) 
+            {
+                reminder += nums[rightIndex];
+                while (reminder >= target) {
+                    reminder -= nums[leftIndex];
+                    resultCount = min(rightIndex - leftIndex + 1, resultCount);
+                    leftIndex++;
+                }
+                rightIndex++;
+            }
+			return resultCount == INT_MAX ? 0 : resultCount;
         }
+
+
 #pragma endregion Main
 
 #pragma region TestData
@@ -59,8 +77,8 @@ namespace Solution209
         Minimum_Size_Subarray_Sum_Model GetTestData001(void)
         {
             Minimum_Size_Subarray_Sum_Model result;
-            result.target = 7;
-            result.nums = {2, 3, 1, 2, 4, 3};
+            result.target = 213;
+            result.nums = {12,28,83,4,25,26,25,2,25,25,25,12};
             return result;
             //Expect: 2
         };
