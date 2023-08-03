ef4662e039b732461a0e25ea319da6d73dd1b196
gotoa1234 <cap8825@gmail.com>
2023-08-04 00:11:36 +0800

    1. finish 0221

--- Medium/0221.Maximal_Square.cpp
+++ Medium/0221.Maximal_Square.cpp
@@ -49,57 +49,40 @@ namespace Solution221
 #pragma region Main
 	public:
 		/// <summary>
-		///     thinking：		
-		///       Runtime：
-		/// Memory Usage ：
+		///     thinking：DP題，從給定的matrix最右下角開始，並利用兩個Vector<int> 紀錄
+		///               每一列算完後，累計至上一列，符合對角+右+下都成立的情況下才可累加			
+		///       Runtime：  63 ms Beats 98.79 %
+		/// Memory Usage ：18.7 MB Beats 95.40 %
 		/// </summary>
 		int maximalSquare(vector<vector<char>>& matrix) {
-			auto maxSquareValue = 0;
-			for (int xAxis = 0; xAxis < matrix.size(); xAxis++)
+			int xAxisMax = matrix.size();
+			int yAxisMax = matrix[0].size();
+			vector<int> preVecs(yAxisMax + 1, 0);
+			vector<int> currVecs(yAxisMax + 1, 0);
+			int result = 0;
+			//1. 最右下開始計算
+			for (int xAxis = xAxisMax - 1; xAxis >= 0; xAxis--) 
 			{
-				for (int yAxis = 0; yAxis < matrix[xAxis].size(); yAxis++)
-				{
-					if (matrix[xAxis][yAxis] == 1)
+				for (int yAxis = yAxisMax - 1; yAxis >= 0; yAxis--) 
+				{				
+					int right = currVecs[yAxis + 1];//右是看當前列
+					int down = preVecs[yAxis];//下是看前一列
+					int diagonal = preVecs[yAxis + 1];//右下也是看前一列
+					//2-1. 如果右+下+右下(對角)都有值，表示矩陣成立，進行累計
+					if (matrix[xAxis][yAxis] == '1') 
 					{
-						auto getLevel = ExtendedExploration(xAxis, yAxis, 1, xAxis + 1, yAxis + 1, matrix);
-						maxSquareValue = max(maxSquareValue, getLevel);
-					}
+						
+						currVecs[yAxis] = 1 + min(right, min(diagonal, down));
+						result = max(result, currVecs[yAxis]);
+					}//2-2. 如果不是'1'時，直接設為0
+					else 
+						currVecs[yAxis] = 0;					
 				}
+				preVecs = currVecs;
 			}
-			return maxSquareValue;
+			//3. 矩陣n*n 所以回傳為平方
+			return result * result;
 		}
-
-		int ExtendedExploration(int origineX, int origineY, int level, int xAxisMax, int yAxisMax, vector<vector<char>>& matrix)
-		{						
-			if (xAxisMax >= matrix.size() || yAxisMax >= matrix[origineX].size())
-				return level;
-
-			bool isSquare = true;					
-			for (int xAxis = origineX + level, yAxis = origineY; yAxis < yAxisMax; yAxis++)
-			{
-				if (matrix[xAxis][yAxis] == 0)
-				{
-					isSquare = false;
-					break;
-				}
-			}
-			if(isSquare)
-		    for (int xAxis = origineX, yAxis = origineY + level; xAxis < xAxisMax; xAxis++)
-			{				
-				if (matrix[xAxis][yAxis] == 0)
-				{
-					isSquare = false;
-					break;
-				}
-			}
-			if (isSquare)
-			{
-				level++;
-				level = ExtendedExploration(origineX, origineY, level, xAxisMax + level, yAxisMax + level, matrix);
-			}
-			return level;
-		}
-
 #pragma endregion Main
 
 #pragma region TestData
@@ -136,6 +119,38 @@ namespace Solution221
 			Maximal_Square_Model result;
 			result.matrix = { {'0'} };
 			return result;//except: 0
+		};
+
+		/// <summary>
+		/// test data 4
+		/// </summary>   
+		Maximal_Square_Model GetTestData004(void)
+		{
+			Maximal_Square_Model result;
+			result.matrix = { {'1','1','1','1','0'},
+				              {'1','1','1','1','0'},
+				              {'1','1','1','1','1'},
+				              {'1','1','1','1','1'},
+				              {'0','0','1','1','1'}};
+			return result;//except: 16
+		};
+
+		/// <summary>
+		/// test data 5
+		/// </summary>   
+		Maximal_Square_Model GetTestData005(void)
+		{
+			Maximal_Square_Model result;
+			result.matrix = { {'0','0','0','1','0','1','1','1'},
+				              {'0','1','1','0','0','1','0','1'},
+				              {'1','0','1','1','1','1','0','1'},
+				              {'0','0','0','1','0','0','0','0'},
+				              {'0','0','1','0','0','0','1','0'},
+				              {'1','1','1','0','0','1','1','1'},
+				              {'1','0','0','1','1','0','0','1'},
+				              {'0','1','0','0','1','1','0','0'},
+				              {'1','0','0','1','0','0','0','0'} };
+			return result;//except: 1
 		};
 #pragma endregion TestData
 
